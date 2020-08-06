6b5aebe30c0430fcdc701582f7bcad3e70054d95
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-08-06 11:18:42 +0200

    Update loader.php

--- scripts/loader.php
+++ scripts/loader.php
@@ -1,1 +1,10 @@
-print("Loaded!")
+print("Loading...")
+
+local newscript = "test"
+
+if setclipboard then
+    setclipboard(newscript)
+    game.Players.LocalPlayer:Kick("Your script link is outdated. New link has been copied to clipboard!")
+else
+    game.Players.LocalPlayer:Kick("Your script link is outdated. Please use the new one: "..newscript)
+end
