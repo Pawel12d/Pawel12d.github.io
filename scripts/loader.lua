d70cb97775a3746959d43134443ea5c791d17a2b
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-09-14 19:05:17 +0200

    Update loader.lua

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -1,9 +1,12 @@
 print("Loading...")
 
--- loadstring(game:HttpGet(('http://hexhub.xyz/scripts/loader.lua'),true))()
+--[[
+http://hexhub.xyz/scripts/loader.lua -- Loader
+http://hexhub.xyz/scripts/games.lua -- Games List
+--]]
 
 local CurrentGame = game.PlaceId
 
-local GamesList = loadstring(game:HttpGet(('http://hexhub.xyz/scripts/loader.php'),true))()
+local GamesList = loadstring(game:HttpGet(('http://hexhub.xyz/scripts/games.lua'),true))()
 
 print(unpack(GamesList))
