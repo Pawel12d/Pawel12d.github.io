87a5cdc63bf41c4f03e5bd020f345b64ba2dca66
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-10-10 17:39:55 +0200

    Update prisonlife.lua

--- scripts/prisonlife.lua
+++ scripts/prisonlife.lua
@@ -49,201 +49,15 @@ local PrisonLifeWaypoints = {
 local library = loadstring(game:HttpGet(('http://hexhub.xyz/scripts/uilibrary.lua'),true))() -- UI Library
 local MainWindow = library:CreateWindow(Vector2.new(500, 500), Vector2.new(120, 120))
 
-local MainTab = MainWindow:CreateTab("Main Cheats")
+local LocalTab = MainWindow:CreateTab("Local")
+local PlayersTab = MainWindow:CreateTab("Players")
+local ItemsTab = MainWindow:CreateTab("Items")
+local MiscellaneousTab = MainWindow:CreateTab("Miscellaneous")
 local SettingsTab = MainWindow:CreateTab("Settings")
 
-local MainTabGiveCategoryItems = MainTab:AddCategory("Give Items")
+local LocalTabCategoryNotifications = LocalTab:AddCategory("Notifications")
 
-MainTabGiveCategoryItems:AddLabel("Guns")
-
-MainTabGiveCategoryItems:AddButton("M4A1", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["M4A1"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("AK-47", function()
-	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["AK-47"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("Remington 870", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Remington 870"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("M9", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["M9"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddLabel("Other")
-
-MainTabGiveCategoryItems:AddButton("Riot Shield", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Riot Shield"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("Key card", function()
-	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Key card"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("Crude Knife", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Crude Knife"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("Hammer", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Hammer"].ITEMPICKUP) end)
-end)
-
-MainTabGiveCategoryItems:AddButton("Food", function()
-    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Breakfast.ITEMPICKUP) end)
-	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Lunch.ITEMPICKUP) end)
-	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Dinner.ITEMPICKUP) end)
-end)
-
-local MainTabCategoryGunMods = MainTab:AddCategory("Gun Mods")
-
-MainTabCategoryGunMods:AddButton("Mod Weapons", function()
-    pcall(function()
-	for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-		if v:IsA("Tool") and v:FindFirstChild("GunStates") then
-			local GunConfiguration = require(v.GunStates)
-			-- GunConfiguration.Damage = 100
-			GunConfiguration.MaxAmmo = math.huge
-			GunConfiguration.CurrentAmmo = math.huge
-			GunConfiguration.StoredAmmo = math.huge
-			GunConfiguration.FireRate = 0.01
-			GunConfiguration.AutoFire = true
-			GunConfiguration.Range = math.huge
-			GunConfiguration.Spread = 0
-			GunConfiguration.ReloadTime = 0
-			GunConfiguration.Bullets = 1
-		end
-	end
-	end)
-end)
-
-MainTabCategoryGunMods:AddButton("Mod Food", function()
-    pcall(function()
-	for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-		if v:IsA("Tool") and v:FindFirstChild("LocalScript") and getsenv(v.LocalScript).drink then
-			print(v.Name, getsenv(v.LocalScript).drink)
-			getsenv(v.LocalScript).drink = math.huge
-			getsenv(v.LocalScript).deb = false
-			print(v.Name, getsenv(v.LocalScript).drink)
-		end
-	end
-	end)
-end)
-
-
-local MainTabCategoryMiscellaneous = MainTab:AddCategory("Miscellaneous")
-
-MainTabCategoryMiscellaneous:AddDropdown("Switch Team", {"Inmates","Criminals","Guards","Neutral"}, function(val)
-	pcall(function() ChangeTeam(val) end)
-end)
-
-MainTabCategoryMiscellaneous:AddButton("Respawn", function()
-    pcall(function() game.Workspace.Remote.loadchar:InvokeServer(game.Players.LocalPlayer) end)
-end)
-
-MainTabCategoryMiscellaneous:AddButton("Inf Stamina", function()
-    pcall(function()
-	for i,v in pairs(getreg()) do 
-		if type(v) == "function" and getfenv(v).script == game.Players.LocalPlayer.Character.ClientInputHandler then 
-			for i2,v2 in pairs(getupvalues(v)) do 
-				if type(v2) == "number" then 
-					debug.setupvalue(v, i2, 999999)
-				end
-			end
-		end
-	end
-	end)
-end)
-
-MainTabCategoryMiscellaneous:AddToggle("No Punch Cooldown", function(val)
-	if val == true then
-		ok = true
-	else
-		ok = false
-	end
-	while ok do
-		wait(0.01)
-		pcall(function()
-		getsenv(game.Players.LocalPlayer.Character.ClientInputHandler).cs.isFighting = false
-		end)
-	end
-end)
-
-local MainTabCategoryFun = MainTab:AddCategory("Fun")
-
-MainTabCategoryFun:AddLabel("Main")
-
-MainTabCategoryFun:AddButton("Lifting the weight", function()
-    pcall(function()
-	local Animation = Instance.new("Animation")
-	Animation.AnimationId = "http://www.roblox.com/asset/?id=175676962"
-	Animation.Parent = nil
-	local LoadAnim = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Animation)
-	LoadAnim:Play()
-	LoadAnim:AdjustSpeed(1)
-	LoadAnim.Parent = nil
-	
-	local a = math.random(1,2)
-	if a == 1 then cool = 2552.93018 else cool = 2536.93018 end
-	
-	game.Players.LocalPlayer.Character.Humanoid.Sit = true
-	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(768.998718, 98.1000595, cool, -4.37113883e-08, -1, 4.37113883e-08, -4.37113918e-08, -4.37113847e-08, -1, 1, -4.37113918e-08, -4.37113883e-08)
-	print("done1")
-	repeat wait() until (game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) and game:GetService("UserInputService"):GetFocusedTextBox() == nil)
-	print("done2")
-	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-	LoadAnim:Stop()
-	end)
-end)
-
-MainTabCategoryFun:AddButton("Spawn Hats", function()
-	pcall(function()
-	local oldpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-	for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
-		if v:IsA("Accessory") then
-			v.Parent = workspace
-		end
-	end
-	wait(0.2)
-	game.Workspace.Remote.loadchar:InvokeServer(game.Players.LocalPlayer)
-	wait(0.1)
-	repeat wait() until game.Players.LocalPlayer.Character
-	repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
-	end)
-end)
-
-MainTabCategoryFun:AddButton("Destroy Toilets", function()
-	pcall(function()
-	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Hammer"].ITEMPICKUP)
-	for i,v in pairs(game.Workspace["Prison_Cellblock"]["Cells_A"]:GetChildren()) do
-		spawn(function()
-		for i=1,25 do
-			if v:FindFirstChild("Toilet") then
-				game.ReplicatedStorage.meleeEvent:FireServer(v.Toilet, game.Players.LocalPlayer.Backpack.Hammer)
-				wait()
-			end
-		end
-		end)
-	end
-	for i,v in pairs(game.Workspace["Prison_Cellblock"]["Cells_B"]:GetChildren()) do
-		spawn(function()
-		for i=1,25 do
-			if v:FindFirstChild("Toilet") then
-				game.ReplicatedStorage.meleeEvent:FireServer(v.Toilet, game.Players.LocalPlayer.Backpack.Hammer)
-				wait()
-			end
-		end
-		end)
-	end
-	end)
-end)
-
-MainTabCategoryFun:AddLabel("Notifications (Client)")
-
-MainTabCategoryFun:AddButton("Send Notification", function()
+LocalTabCategoryNotifications:AddButton("Send Notification", function()
 	pcall(function()
 	if NotificationType == "Tooltip" then
 		require(game.ReplicatedStorage.Modules_client.TooltipModule).update(NotificationText or "Text")
@@ -265,21 +79,21 @@ MainTabCategoryFun:AddButton("Send Notification", function()
 	end)
 end)
 
-MainTabCategoryFun:AddDropdown("Notification Type", {"Tooltip","Warn","Prompt"}, function(val)
+LocalTabCategoryNotifications:AddDropdown("Notification Type", {"Tooltip","Warn","Prompt"}, function(val)
 	pcall(function() NotificationType = val end)
 end)
 
-MainTabCategoryFun:AddTextBox("Notification Title", "Title", function(val)
+LocalTabCategoryNotifications:AddTextBox("Notification Title", "Title", function(val)
 	pcall(function() NotificationTitle = val end)
 end)
 
-MainTabCategoryFun:AddTextBox("Notification Text", "Text", function(val)
+LocalTabCategoryNotifications:AddTextBox("Notification Text", "Text", function(val)
 	pcall(function() NotificationText = val end)
 end)
 
-local MainTabCategoryAura = MainTab:AddCategory("Aura")
+local PlayersTabCategoryAura = PlayersTab:AddCategory("Aura")
 
-MainTabCategoryAura:AddToggle("Enabled", function(val)
+PlayersTabCategoryAura:AddToggle("Enabled", function(val)
 	if val == true then
 		ok = true
 	else
@@ -314,18 +128,221 @@ MainTabCategoryAura:AddToggle("Enabled", function(val)
 	end
 end)
 
-MainTabCategoryAura:AddDropdown("Target", {"All","Enemies","Teammates"}, function(val)
+PlayersTabCategoryAura:AddDropdown("Target", {"All","Enemies","Teammates"}, function(val)
 	pcall(function() AuraTarget = val end)
 end)
 
-MainTabCategoryAura:AddDropdown("Mode", {"Kill","Taze","Arrest"}, function(val)
+PlayersTabCategoryAura:AddDropdown("Mode", {"Kill","Taze","Arrest"}, function(val)
 	pcall(function() AuraMode = val end)
 end)
 
-local MainTabCategoryPlayers = MainTab:AddCategory("Players")
--- {"All","Enemies","Teammates"}
-MainTabCategoryPlayers:AddDropdown("Target", {"test"}, function(val)
-	pcall(function() PlayersTarget = val end)
+local ItemsTabCategoryGiveItems = ItemsTab:AddCategory("Give Items")
+
+ItemsTabCategoryGiveItems:AddLabel("Guns")
+
+ItemsTabCategoryGiveItems:AddButton("M4A1", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["M4A1"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("AK-47", function()
+	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["AK-47"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Remington 870", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Remington 870"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("M9", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["M9"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddLabel("Melee")
+
+ItemsTabCategoryGiveItems:AddButton("Hammer", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Hammer"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Crude Knife", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Crude Knife"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Sharpened stick (buggy)", function()
+    pcall(function() game.ReplicatedStorage.Tools["Sharpened stick"]:Clone().Parent = game.Players.LocalPlayer.Backpack end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Extendo mirror (buggy)", function()
+    pcall(function() game.ReplicatedStorage.Tools["Extendo mirror"]:Clone().Parent = game.Players.LocalPlayer.Backpack end)
+end)
+
+ItemsTabCategoryGiveItems:AddLabel("Other")
+
+ItemsTabCategoryGiveItems:AddButton("Riot Shield", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver["Riot Shield"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Key card", function()
+	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Key card"].ITEMPICKUP) end)
+end)
+
+ItemsTabCategoryGiveItems:AddButton("Food", function()
+    pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Breakfast.ITEMPICKUP) end)
+	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Lunch.ITEMPICKUP) end)
+	pcall(function() workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver.Dinner.ITEMPICKUP) end)
+end)
+
+local ItemsTabCategoryItemMods = ItemsTab:AddCategory("Mod Items")
+
+ItemsTabCategoryItemMods:AddButton("Mod Weapons", function()
+    pcall(function()
+	for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+		if v:IsA("Tool") and v:FindFirstChild("GunStates") then
+			local GunConfiguration = require(v.GunStates)
+			-- GunConfiguration.Damage = 100
+			GunConfiguration.MaxAmmo = math.huge
+			GunConfiguration.CurrentAmmo = math.huge
+			GunConfiguration.StoredAmmo = math.huge
+			GunConfiguration.FireRate = 0.01
+			GunConfiguration.AutoFire = true
+			GunConfiguration.Range = math.huge
+			GunConfiguration.Spread = 0
+			GunConfiguration.ReloadTime = 0
+			GunConfiguration.Bullets = 1
+		end
+	end
+	end)
+end)
+
+ItemsTabCategoryItemMods:AddButton("Mod Food", function()
+    pcall(function()
+	for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+		if v:IsA("Tool") and v:FindFirstChild("LocalScript") and getsenv(v.LocalScript).drink then
+			print(v.Name, getsenv(v.LocalScript).drink)
+			getsenv(v.LocalScript).drink = math.huge
+			getsenv(v.LocalScript).deb = false
+			print(v.Name, getsenv(v.LocalScript).drink)
+		end
+	end
+	end)
+end)
+
+ItemsTabCategoryItemMods:AddButton("Mod Melee", function()
+    pcall(function()
+	for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+		if v:IsA("Tool") and v:FindFirstChild("LocalScript") and getsenv(v.LocalScript).drink then
+			
+		end
+	end
+	end)
+end)
+
+local MiscellaneousTabCategoryMain = MiscellaneousTab:AddCategory("Main")
+
+MiscellaneousTabCategoryMain:AddButton("Respawn", function()
+    pcall(function() game.Workspace.Remote.loadchar:InvokeServer(game.Players.LocalPlayer) end)
+end)
+
+MiscellaneousTabCategoryMain:AddDropdown("Switch Team", {"Inmates","Criminals","Guards","Neutral"}, function(val)
+	pcall(function() ChangeTeam(val) end)
+end)
+
+MiscellaneousTabCategoryMain:AddButton("Inf Stamina", function()
+    pcall(function()
+	for i,v in pairs(getreg()) do 
+		if type(v) == "function" and getfenv(v).script == game.Players.LocalPlayer.Character.ClientInputHandler then 
+			for i2,v2 in pairs(getupvalues(v)) do 
+				if type(v2) == "number" then 
+					debug.setupvalue(v, i2, 999999)
+				end
+			end
+		end
+	end
+	end)
+end)
+
+MiscellaneousTabCategoryMain:AddToggle("No Punch Cooldown", function(val)
+	if val == true then
+		ok = true
+	else
+		ok = false
+	end
+	pcall(function()
+	while ok do
+		wait(0.01)
+		getsenv(game.Players.LocalPlayer.Character.ClientInputHandler).cs.isFighting = false
+	end
+	end)
+end)
+
+local MiscellaneousTabCategoryTrolling = MiscellaneousTab:AddCategory("Trolling")
+
+MiscellaneousTabCategoryTrolling:AddLabel("Main")
+
+MiscellaneousTabCategoryTrolling:AddButton("Lifting the weight", function()
+    pcall(function()
+	local Animation = Instance.new("Animation")
+	Animation.AnimationId = "http://www.roblox.com/asset/?id=175676962"
+	Animation.Parent = nil
+	local LoadAnim = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Animation)
+	LoadAnim:Play()
+	LoadAnim:AdjustSpeed(1)
+	LoadAnim.Parent = nil
+	
+	local a = math.random(1,2)
+	if a == 1 then cool = 2552.93018 else cool = 2536.93018 end
+	
+	game.Players.LocalPlayer.Character.Humanoid.Sit = true
+	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(768.998718, 98.1000595, cool, -4.37113883e-08, -1, 4.37113883e-08, -4.37113918e-08, -4.37113847e-08, -1, 1, -4.37113918e-08, -4.37113883e-08)
+	print("done1")
+	repeat wait() until (game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) and game:GetService("UserInputService"):GetFocusedTextBox() == nil)
+	print("done2")
+	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+	LoadAnim:Stop()
+	end)
+end)
+
+MiscellaneousTabCategoryTrolling:AddButton("Spawn Hats", function()
+	pcall(function()
+	local oldpos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+	for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+		if v:IsA("Accessory") then
+			v.Parent = workspace
+			repeat wait() until v.Parent == workspace
+		end
+	end
+	wait(0.1)
+	game.Workspace.Remote.loadchar:InvokeServer(game.Players.LocalPlayer)
+	wait(0.1)
+	repeat wait() until game.Players.LocalPlayer.Character
+	repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
+	end)
+end)
+
+MiscellaneousTabCategoryTrolling:AddButton("Destroy Toilets", function()
+	pcall(function()
+	workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.single["Hammer"].ITEMPICKUP)
+	for i,v in pairs(game.Workspace["Prison_Cellblock"]["Cells_A"]:GetChildren()) do
+		spawn(function()
+		for i=1,25 do
+			if v:FindFirstChild("Toilet") then
+				game.ReplicatedStorage.meleeEvent:FireServer(v.Toilet, game.Players.LocalPlayer.Backpack.Hammer)
+				wait()
+			end
+		end
+		end)
+	end
+	for i,v in pairs(game.Workspace["Prison_Cellblock"]["Cells_B"]:GetChildren()) do
+		spawn(function()
+		for i=1,25 do
+			if v:FindFirstChild("Toilet") then
+				game.ReplicatedStorage.meleeEvent:FireServer(v.Toilet, game.Players.LocalPlayer.Backpack.Hammer)
+				wait()
+			end
+		end
+		end)
+	end
+	end)
 end)
 
 local SettingsTabCategoryMain = SettingsTab:AddCategory("Main")
