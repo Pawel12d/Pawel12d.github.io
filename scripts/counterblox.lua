c4d66081dd75a088c62666b5f346ad29d7214474
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-10-12 13:52:32 +0200

    Update counterblox.lua

--- scripts/counterblox.lua
+++ scripts/counterblox.lua
@@ -1,3 +1,36 @@
 print("Counter Blox Script Loaded!")
-local cfgnigga = game:GetService("HttpService"):JSONDecode(tostring(getgenv().HexHubCFG))
-print(unpack(cfgnigga))
+
+local mt = getrawmetatable(game)
+local oldNamecall = mt.__namecall
+
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+	local callingscript = getcallingscript()
+    local args = {...}
+	
+    if method == "kick" then
+		print("detection1")
+        return wait(99e99)
+	elseif args[1] == game.Players.LocalPlayer.UserId then
+		print("detection 2")
+		return wait(99e99)
+    elseif method == "SetPrimaryPartCFrame" then
+
+    elseif method == "FindPartOnRayWithWhitelist" then
+
+    elseif method == "FindPartOnRayWithIgnoreList" then
+
+    elseif method == "InvokeServer" then
+        if self.Name == "Hugh" then
+			print("detection 3")
+            return wait(99e99)
+        end
+    elseif method == "FireServer" then
+		if self.Name == "DataEvent" and args[1][4] and IsInventoryUnlocked then
+			
+		end
+    end
+    return oldNamecall(self, unpack(args))
+end)
