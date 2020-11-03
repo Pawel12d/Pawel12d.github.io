61a0f5e6f47ef578ae696b15c0598a330270381c
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-11-03 13:20:24 +0100

    Update counterblox.lua

--- scripts/counterblox.lua
+++ scripts/counterblox.lua
@@ -281,6 +281,7 @@ mt.__namecall = newcclosure(function(self, ...)
 	elseif method == "FindPartOnRayWithIgnoreList" then
 		if callingscript == game.Players.LocalPlayer.PlayerGui.Client and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
 			if getgenv().HexHubSettings.permsettings.aimbotbase.Enabled == true and SilentAimTarget ~= nil then
+				print("hugh")
 				args[2] = Ray.new(CurrentCamera.CFrame.p, (SilentAimTarget - CurrentCamera.CFrame.p).unit * 2048)
 			end
 		end
