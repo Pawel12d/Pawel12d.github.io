c604c2a8b7548b927a9cf20e7d4a69945947b2fc
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-11-03 16:37:06 +0100

    Update counterblox.lua

--- scripts/counterblox.lua
+++ scripts/counterblox.lua
@@ -363,7 +363,8 @@ mt.__namecall = newcclosure(function(self, ...)
 	elseif method == "FindPartOnRayWithIgnoreList" then
 		if SilentAimAiming == true and SilentAimTarget ~= nil and callingscript == game.Players.LocalPlayer.PlayerGui.Client and getgenv().HexHubSettings.permsettings.aimbotbase.Enabled == true then
 			if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
-				args[1] = Ray.new(CurrentCamera.CFrame.p, (SilentAimTarget - CurrentCamera.CFrame.p).unit * 2048) -- game.ReplicatedStorage.Weapons[game.Players.LocalPlayer.Character.EquippedTool.Value].Range.Value
+				args[1] = Ray.new(CurrentCamera.CFrame.Position, (SilentAimTarget - CurrentCamera.CFrame.Position).unit * 2048) -- game.ReplicatedStorage.Weapons[game.Players.LocalPlayer.Character.EquippedTool.Value].Range.Value
+				-- args[2] = Ray.new(Camera.CFrame.Position, (silentaimtarget.Character[LegitbotAimbotTargetPart].CFrame.p - Camera.CFrame.Position).unit * 2048)
 			end
 		end
 	elseif method == "InvokeServer" then
