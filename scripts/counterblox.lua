35063bae1378f0e9b2304de1e9f4e74145108fa5
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-11-04 13:51:28 +0100

    Update counterblox.lua

--- scripts/counterblox.lua
+++ scripts/counterblox.lua
@@ -587,7 +587,8 @@ mt.__namecall = newcclosure(function(self, ...)
 		local oldtar = getgenv().HexHubSettings.permsettings.aimbotbase.silentaimtarget or false
 		if oldtar ~= false and callingscript == game.Players.LocalPlayer.PlayerGui.Client and getgenv().HexHubSettings.permsettings.aimbotbase.Enabled == true then
 			if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
-				args[1] = Ray.new(workspace.CurrentCamera.CFrame.Position, (oldtar - workspace.CurrentCamera.CFrame.Position).unit * 2048) -- game.ReplicatedStorage.Weapons[game.Players.LocalPlayer.Character.EquippedTool.Value].Range.Value
+				print("Silent Aim", oldtar)
+				-- args[1] = Ray.new(workspace.CurrentCamera.CFrame.Position, (oldtar - workspace.CurrentCamera.CFrame.Position).unit * 2048) -- game.ReplicatedStorage.Weapons[game.Players.LocalPlayer.Character.EquippedTool.Value].Range.Value
 				-- args[2] = Ray.new(Camera.CFrame.Position, (silentaimtarget.Character[LegitbotAimbotTargetPart].CFrame.p - Camera.CFrame.Position).unit * 2048)
 			end
 		end
