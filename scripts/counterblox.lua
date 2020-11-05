a31d10df7c5338e0adbd8a0fb4e72b5e2efa5c8c
Pawel12d <68199836+Pawel12d@users.noreply.github.com>
2020-11-05 10:04:13 +0100

    Update counterblox.lua

--- scripts/counterblox.lua
+++ scripts/counterblox.lua
@@ -57,6 +57,104 @@ end
 local mouse = game:GetService("Players").LocalPlayer:GetMouse()
 local CurrentCamera = workspace.CurrentCamera
 
+FLYING = false
+flyspeed = 1
+QEfly = false
+
+local mouse = game:GetService("Players").LocalPlayer:GetMouse()
+
+function sFLY(toggle)
+	repeat wait() until game:GetService("Players").LocalPlayer and game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild('HumanoidRootPart') and game:GetService("Players").LocalPlayer.Character:FindFirstChild('Humanoid')
+	repeat wait() until mouse
+
+	local T = game:GetService("Players").LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+	local CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+	local lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+	local SPEED = 0
+
+    if toggle == false then
+        FLYING = false
+        game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
+    elseif toggle == true then
+        local function FLY()
+            FLYING = true
+
+            local BG = Instance.new('BodyGyro', T)
+            local BV = Instance.new('BodyVelocity', T)
+
+            BG.P = 9e4
+            BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+            BG.cframe = T.CFrame
+            BV.velocity = Vector3.new(0, 0, 0)
+            BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+            
+            spawn(function()
+                repeat wait()
+                    if game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
+                        game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = true
+                    end
+                    if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0 then
+                        SPEED = 50
+                    elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0) and SPEED ~= 0 then
+                        SPEED = 0
+                    end
+                    if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 or (CONTROL.Q + CONTROL.E) ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                        lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+                    elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and (CONTROL.Q + CONTROL.E) == 0 and SPEED ~= 0 then
+                        BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                    else
+                        BV.velocity = Vector3.new(0, 0, 0)
+                    end
+                    BG.cframe = workspace.CurrentCamera.CoordinateFrame
+                until not FLYING
+                CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+                lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+                SPEED = 0
+                BG:destroy()
+                BV:destroy()
+                if game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
+                end
+            end)
+        end
+
+        mouse.KeyDown:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = flyspeed
+            elseif KEY:lower() == 's' then
+                CONTROL.B = - flyspeed
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = - flyspeed
+            elseif KEY:lower() == 'd' then 
+                CONTROL.R = flyspeed
+            elseif QEfly and KEY:lower() == 'e' then
+                CONTROL.Q = flyspeed*2
+            elseif QEfly and KEY:lower() == 'q' then
+                CONTROL.E = -flyspeed*2
+            end
+        end)
+
+        mouse.KeyUp:connect(function(KEY)
+            if KEY:lower() == 'w' then
+                CONTROL.F = 0
+            elseif KEY:lower() == 's' then
+                CONTROL.B = 0
+            elseif KEY:lower() == 'a' then
+                CONTROL.L = 0
+            elseif KEY:lower() == 'd' then
+                CONTROL.R = 0
+            elseif KEY:lower() == 'e' then
+                CONTROL.Q = 0
+            elseif KEY:lower() == 'q' then
+                CONTROL.E = 0
+            end
+        end)
+
+        FLY()
+    end
+end
+
 local function PLR_VISIBLE(plr)
 	local IgnoreList = {game:GetService("Players").LocalPlayer.Character}
 	local NewRay = Ray.new(CurrentCamera.CFrame.p, (plr.Character.HumanoidRootPart.Position - CurrentCamera.CFrame.p).unit * 2048)
@@ -168,6 +266,15 @@ local function ANTIAIMBOT_LOOP()
 	end)
 end
 
+local function NOCLIP_LOOP()
+    if game.Players.LocalPlayer.Character then
+        for _, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+            if v:IsA("BasePart") and v.CanCollide == true then
+                v.CanCollide = false
+            end
+        end
+    end
+end
 
 local function KILL_LOOP(plrs)
 	pcall(function()
@@ -531,6 +638,35 @@ MiscellaneousTabCategoryMain:AddToggle("Disable Chat Filter", false, function(va
 	end)
 end)
 
+local MiscellaneousTabCategoryItems = MiscellaneousTab:AddCategory("Items")
+
+MiscellaneousTabCategoryItems:AddToggle("Inf Cash", false, function(val)
+	pcall(function()
+		getgenv().HexHubSettings.tempsettings.counterblox.InfCash = val
+		game.Players.LocalPlayer.Cash.Value = 10000
+	end)
+end)
+
+local MiscellaneousTabCategoryMovement = MiscellaneousTab:AddCategory("Movement")
+
+MiscellaneousTabCategoryMovement:AddToggle("Allow Reset Character", false, function(val)
+	pcall(function()
+		game:GetService("StarterGui"):SetCore("ResetButtonCallback", val)
+	end)
+end)
+
+MiscellaneousTabCategoryMovement:AddToggle("Noclip", false, function(val)
+	pcall(function()
+		if val == true then
+			Noclipping = game:GetService("RunService").Stepped:connect(NOCLIP_LOOP)
+			sFLY(true)
+		else
+			Noclipping:Disconnect()
+			sFLY(false)
+		end
+	end)
+end)
+
 local MiscellaneousTabCategoryBypasses = MiscellaneousTab:AddCategory("Bypasses")
 
 MiscellaneousTabCategoryBypasses:AddToggle("No Fall Damage", false, function(val)
@@ -568,6 +704,12 @@ cbfirebullethook = hookfunc(cbClient.firebullet, function(...)
     return cbfirebullethook(unpack(args))
 end)
 --]]
+
+game.Players.LocalPlayer.Cash.Chaged:Connect(function()
+	if getgenv().HexHubSettings.tempsettings.counterblox.InfCash and game.Players.LocalPlayer.Cash.Value ~= 10000 then
+		game.Players.LocalPlayer.Cash.Value = 10000
+	end
+end)
 
 CurrentCamera.ChildAdded:Connect(function(child)
 	spawn(function()
